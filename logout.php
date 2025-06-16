bf268cada24f76b88f52573a47840fcbb508f97e
andremarthinusl <2311035@students.universitasmulia.ac.id>
2025-06-16 20:20:07 +0800

    Update redirect

--- logout.php
+++ logout.php
@@ -8,46 +8,6 @@ $_SESSION = array();
 session_destroy();
 
 // Redirect to login page
-header("Location: login.php");
+header("Location: login");
 exit;
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
